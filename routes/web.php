bf4078a6594ea6082789b62a088fddb5476bdc0b
dapr06 <danielaprato12@gmail.com>
2023-04-25 15:49:57 +0200

    cambios en el web.php por que no funcionaba 25/04/23

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\WorkerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,12 +31,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('workers', WorkerController::class);
+    Route::resource('vehicles', VehicleController::class);
+    Route::resource('services', ServiceController::class);
+    Route::resource('clients', ClientController::class);
+    Route::resource('bookings', BookingController::class);
+
 });
 
 require __DIR__.'/auth.php';
 
-Route::resource('workers', WorkerController::class);
-Route::resource('vehicles', VehicleController::class);
-Route::resource('services', ServiceController::class);
-Route::resource('clients', ClientController::class);
-Route::resource('bookings', BookingController::class);
+
+

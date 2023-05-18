bc2f73ed559de3d3ca3e67632032ad30009cfba8
dapr06 <danielaprato12@gmail.com>
2023-05-18 17:52:05 +0200

    18/05/2023 dentro de perfil provisional

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,4 +1,4 @@
-<div class="menu">
+<div class="menu" style="display: flex; flex-wrap: nowrap; align-items: center;">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <a class="menua" href="{{ url('/') }}"><img src="/img/chapuzonLogo.png" width="120" height="40"></a>
     <a class="menua" href="{{ url('/') }}">Nosotros</a>
@@ -10,11 +10,31 @@
     @if (Route::has('login'))
         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
             @auth
-                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                <div >
+                    <div>
+                        <div>{{ Auth::user()->name }}</div>
+                    </div>
+                    <div >
+                        <x-responsive-nav-link :href="route('profile.edit')">
+                            {{ __('Perfil') }}
+                        </x-responsive-nav-link>
+                    </div>
+                        <!-- Authentication -->
+                    <div>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-responsive-nav-link :href="route('logout')"
+                                                   onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-responsive-nav-link>
+                        </form>
+                    </div>
+                </div>
             @else
-                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                <a href="{{ route('login') }}"><img src="/img/acceso.png" class="inicios"></a>
             @endauth
         </div>
     @endif
-
 </div>

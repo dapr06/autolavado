3139c78dec4c9a76db9da36bc4aef49396830335
cristina <cristina@gmail.com>
2023-05-18 18:32:41 +0200

    18/05/23 - cambios servicios

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -1,33 +1,29 @@
-<x-menu></x-menu>
-    <h2>Servicios</h2>
-    <table border="1">
-        <tr>
+<x-menu/>
+    <div class="container">
+        <h1 class="text-center">¡¡Nuestros Servicios!!</h1><br>
+        <p class="text-justify">
+            Chapuzón es una empresa especializada en la limpieza de vehículos, tanto en su exterior como en su
+            interior. Contamos con un equipo de profesionales altamente capacitados y utilizamos productos de
+            calidad para garantizar resultados excepcionales.</p>
+        <div class="row">
+            @foreach ($services as $service)
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ $service->name}}</h4>
+                                <p class="card-text text-justify">{{ $service->description }}</p>
+                                <p>Precio: {{ $service->price}}€</p>
+                                <p>Duración aproximada: {{ $service->time}}min</p>
+                                <p>Botón edit admin: <a href='{{ route('services.edit', $service) }}'>X</a></p>
+                            </div>
+                        </div>
+                    </div>
+            @endforeach
+        </div>
+    </div>
 
-            <th>Name</th>
-            <th>Description</th>
-            <th>Time</th>
-            <th>Price</th>
-            <th>Eliminar</th>
-        </tr>
 
-        @foreach ($services as $service)
-            <tr>
-
-                <td><a href='{{ route('services.show', $service) }}'>{{ $service->name}}</a></td>
-                <td><a href='{{ route('services.show', $service) }}'>{{ $service->description }}</a></td>
-                <td><a href='{{ route('services.show', $service) }}'>{{ $service->time }}</a></td>
-                <td><a href='{{ route('services.show', $service) }}'>{{ $service->price}}</a></td>
-                <td>
-                    <form action='{{ route('services.destroy', $service) }}' method='post'>
-                        @method('delete')
-                        @csrf
-
-                        <input class='button' type='submit' name='eliminar' value='X'/>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table><br><br>
+   <br><br>
     <button><a href='{{ route('services.create') }}'>Crear servicio</a></button><br><br>
 
 
